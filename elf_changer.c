337a875da6034e3de15f8855cfb4c671e5c6689a
Tristan Gajšek <tristan.gajsek@student.um.si>
2024-03-31 12:43:17 +0200

    Add command line argument handling in main function

---
+++ elf_changer.c
@@ -0,0 +1,30 @@
+#include <elf.h>
+#include <stdio.h>
+
+void printUsage() {
+    printf("elf_loader [-hlc] [OPTIONS] elf_path\n\n"
+           "-h\n  izpis zaglavja zbirke podane v elf_path\n\n"
+           "-l\n  izpis vseh funkcij, ki jih najdete v .text sekciji in imajo "
+           "velikost večjo od 20 zlogov\n\n"
+           "-c [spr1,spr2,spr3,...]\n  spreminjanje vrednosti vseh spremeljivk "
+           "programa za +20, ki jih lahko najdete v programu v elf_path\n");
+}
+
+int main(const int argc, const char **argv) {
+    if (argc < 3) {
+        printUsage();
+        return 0;
+    }
+
+    if (argv[1][0] == '-') {
+        if (argv[1][1] == 'h') {
+            return 0;
+        } else if (argv[1][1] == 'l') {
+            return 0;
+        } else if (argv[1][1] == 'c') {
+            return 0;
+        }
+    }
+
+    printUsage();
+}
