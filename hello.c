e545b7af19ebe4084fbe747eca47d47b22218a2e
Tristan Gajšek <tristan.gajsek@student.um.si>
2024-03-31 12:22:53 +0200

    Initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int funkcija(int a) { return a + 1; }
+
+int g1;
+int g2 = 10;
+int g3 = 14;
+
+int main() {
+    printf("Vrednost g1: %d\n", g1);
+    printf("Vrednost g2: %d\n", g2);
+    printf("Vrednost g3: %d\n", g3);
+    return 0;
+}
