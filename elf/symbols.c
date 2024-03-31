aba08ecda8f84681e491ba3df74e0f2bd0862ebc
Tristan Gajšek <tristan.gajsek@student.um.si>
2024-03-31 13:13:25 +0200

    Add code that prints information about functions that are bigger than 20B

--- elf/symbols.c
+++ elf/symbols.c
@@ -4,7 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-void printSymbols(FILE *file, Elf32_Ehdr header, Elf32_Shdr section) {}
+void printSymbols(FILE *file, Elf32_Ehdr header, Elf32_Shdr section) {
+    Elf32_Sym symbol;
+    char symbolName[32];
+    symbolName[31] = '\0';
+
+    for (unsigned int i = 0; i < section.sh_size / section.sh_entsize; i++) {
+        fseek(file, section.sh_offset + i * section.sh_entsize, SEEK_SET);
+        fread(&symbol, sizeof(Elf32_Sym), 1, file);
+
+        if (((symbol.st_info) & 0xF) == STT_FUNC && symbol.st_size > 20) {
+            if (symbol.st_name != 0) {
+                Elf32_Shdr stringTable;
+                fseek(file,
+                      header.e_shoff + header.e_shentsize * section.sh_link,
+                      SEEK_SET);
+                fread(&stringTable, sizeof(Elf32_Shdr), 1, file);
+
+                fseek(file, stringTable.sh_offset + symbol.st_name, SEEK_SET);
+                fread(symbolName, sizeof(char), 31, file);
+            } else {
+                symbolName[0] = '\0';
+            }
+
+            printf("[%2d]\t0x%08X\t%d\t%s\n", i, symbol.st_value,
+                   symbol.st_size, symbolName);
+        }
+    }
+}
 
 void elf_27259_simboli(const char *path) {
     FILE *file = fopen(path, "r");
