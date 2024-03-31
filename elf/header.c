914d4cd361a64b31dbf8415239826a8b6999f0ce
Tristan Gajšek <tristan.gajsek@student.um.si>
2024-03-31 12:52:45 +0200

    Add function for retrieving magic bytes from ELF header

--- elf/header.c
+++ elf/header.c
@@ -13,6 +13,18 @@ bool getHeader(Elf32_Ehdr *header, const char *path) {
     return true;
 }
 
+void getMagic(Elf32_Ehdr header, char *magic) {
+    sprintf(magic,
+            "%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X "
+            "%02X %02X %02X",
+            header.e_ident[0], header.e_ident[1], header.e_ident[2],
+            header.e_ident[3], header.e_ident[4], header.e_ident[5],
+            header.e_ident[6], header.e_ident[7], header.e_ident[8],
+            header.e_ident[9], header.e_ident[10], header.e_ident[11],
+            header.e_ident[12], header.e_ident[13], header.e_ident[14],
+            header.e_ident[15]);
+}
+
 void printHeader(Elf32_Ehdr header) {}
 
 void elf_27259_glava(const char *path) {
